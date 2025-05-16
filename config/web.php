942a207d06b4a31a924daf9779a20c17453db2df
Arellano <157861661+Arell12@users.noreply.github.com>
2025-05-16 10:08:42 -0600

    Update web.php

--- config/web.php
+++ config/web.php
@@ -337,16 +337,16 @@
         'params' => $params,
     ];
 
-    if (defined('YII_ENV') && YII_ENV !== 'prod') {
-        $config['bootstrap'][] = 'debug';
-        $config['modules']['debug'] = [
-            'class' => 'yii\debug\Module',
-        ];
+    //if (defined('YII_ENV') && YII_ENV !== 'prod') {
+      //  $config['bootstrap'][] = 'debug';
+       // $config['modules']['debug'] = [
+         //   'class' => 'yii\debug\Module',
+        //];
 
-        $config['bootstrap'][] = 'gii';
-        $config['modules']['gii'] = [
-            'class' => 'yii\gii\Module',
-        ];
-    }
+        //$config['bootstrap'][] = 'gii';
+        //$config['modules']['gii'] = [
+          //  'class' => 'yii\gii\Module',
+        //];
+    //}
 
     return $config;
